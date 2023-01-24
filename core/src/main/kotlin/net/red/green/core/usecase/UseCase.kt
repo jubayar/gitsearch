63d5dfaf3aeff4b1435343156c419d811a8d9591
jubayar <jubayar.ict@gmail.com>
2023-01-24 11:09:37 +0800

    Update base view model

--- core/src/main/kotlin/net/red/green/core/usecase/UseCase.kt
+++ core/src/main/kotlin/net/red/green/core/usecase/UseCase.kt
@@ -2,15 +2,11 @@ package net.red.green.core.usecase
 
 abstract class UseCase<Q : UseCase.RequestValues, P : UseCase.ResponseValue> {
     var requestValues: Q? = null
-    var useCaseCallback: UseCaseCallback<P>? = null
 
-    abstract suspend operator fun invoke(requestValues: Q?, callback: UseCaseCallback<P>)
+    abstract suspend operator fun invoke(requestValues: Q?) : UseCaseResponse<ResponseValue, ErrorRes>
+
+    fun getUseCaseName(): String = UseCase::class.java.simpleName
 
     interface RequestValues
     interface ResponseValue
-
-    interface UseCaseCallback<ResponseValue> {
-        fun onSuccessResponse(response: ResponseValue)
-        fun onErrorResponse(errorRes: ErrorRes)
-    }
 }
