b69c3f44b70c661aad218f624c37f38e3d5408af
jubayar <jubayar.ict@gmail.com>
2023-03-08 13:58:31 +0800

    Fixed invoke method

--- user/src/main/kotlin/net/red/green/gitsearch/user/domain/GetUserAccountsUseCase.kt
+++ user/src/main/kotlin/net/red/green/gitsearch/user/domain/GetUserAccountsUseCase.kt
@@ -19,36 +19,31 @@ class GetUserAccountsUseCase(
 
     override suspend fun invoke(
         requestValues: RequestValues?
-    ): UseCaseResponse<UseCase.ResponseValue, ErrorRes> {
+    ): UseCaseResponse<UseCase.ResponseValue, ErrorRes> = withContext(defaultDispatcher) {
+        when (val result = userRepository.getUserAccountList(
+            requestValues!!.queryData.query,
+            requestValues.queryData.page
+        )) {
 
-        withContext(defaultDispatcher) {
-            when (val result = userRepository.getUserAccountList(
-                requestValues!!.queryData.query,
-                requestValues.queryData.page
-            )) {
-
-                is BaseResponse.Success -> {
-                    val accountList = result.body
-                    val accountViewList = GitUsersViewDataMapper.mapToViewData(accountList)
-                    UseCaseResponse.Success(ResponseValue(accountViewList))
-                }
-
-                is BaseResponse.ApiError -> UseCaseResponse.Error(
-                    ErrorRes(
-                        code = result.code,
-                        errorMsgList = listOf(result.errorBody.throwable.message!!)
-                    )
-                )
-
-                is BaseResponse.NetworkError ->
-                    UseCaseResponse.Error(ErrorRes(errorMsgList = listOf(result.error.message!!)))
-
-                is BaseResponse.UnknownError ->
-                    UseCaseResponse.Error(ErrorRes(errorMsgList = listOf(result.error?.message!!)))
+            is BaseResponse.Success -> {
+                val accountList = result.body
+                val accountViewList = GitUsersViewDataMapper.mapToViewData(accountList)
+                UseCaseResponse.Success(ResponseValue(accountViewList))
             }
-        }
 
-        return UseCaseResponse.Error(ErrorRes(errorMsgList = listOf("Something wrong")))
+            is BaseResponse.ApiError -> UseCaseResponse.Error(
+                ErrorRes(
+                    code = result.code,
+                    errorMsgList = listOf(result.errorBody.throwable.message!!)
+                )
+            )
+
+            is BaseResponse.NetworkError ->
+                UseCaseResponse.Error(ErrorRes(errorMsgList = listOf(result.error.message!!)))
+
+            is BaseResponse.UnknownError ->
+                UseCaseResponse.Error(ErrorRes(errorMsgList = listOf(result.error?.message!!)))
+        }
     }
 
     class RequestValues(val queryData: QueryData) : UseCase.RequestValues
